fd792769de1b8c40368916682da33857cdf932cc
McSmallpox <ryanwands18@gmail.com>
2019-04-01 15:13:48 -0600

    completed many to many implementation

--- src/main/java/edu/cnm/deepdive/qod/view/FlatSource.java
+++ src/main/java/edu/cnm/deepdive/qod/view/FlatSource.java
@@ -1,5 +1,6 @@
 package edu.cnm.deepdive.qod.view;
 
+import java.net.URI;
 import java.util.Date;
 import java.util.UUID;
 
@@ -10,4 +11,6 @@ public interface FlatSource {
   Date getCreated();
 
   String getName();
+
+  URI getHref();
 }
